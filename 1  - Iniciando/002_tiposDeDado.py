a8e8735465e182358ada4023419300a04cf00ea7
pedrolisboaa <phlisboa2000@gmail.com>
2024-05-07 17:31:12 -0300

    atualizando

---
+++ 1  - Iniciando/002_tiposDeDado.py
@@ -0,0 +1,9 @@
+umaString = 'Pedro'
+umNumeroString = '123'
+numeroInteiro = 123
+numeroFlutuante = 12.1
+
+print(f'{umaString} é do tipo {type(umaString)}' )
+print(f'{umNumeroString} é do tipo {type(umNumeroString)}' )
+print(f'{numeroInteiro} é do tipo {type(numeroInteiro)}' )
+print(f'{numeroFlutuante} é do tipo {type(numeroFlutuante)}' )
