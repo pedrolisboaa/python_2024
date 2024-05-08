ad4aef42872613ffaede6a043a7168649173b1cb
pedrolisboaa <phlisboa2000@gmail.com>
2024-05-07 23:00:02 -0300

    finalizando o dia

---
+++ 1  - Iniciando/008_condicoes.py
@@ -0,0 +1,9 @@
+entrada = input('Você deseja entrar ou sair?')
+
+
+if entrada == 'entrar':
+    print('Você entrou.')
+elif entrada == 'sair':
+    print(f'Você saiu!')
+else:
+    print('Você não digitou nada.')
