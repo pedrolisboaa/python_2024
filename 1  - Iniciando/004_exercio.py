174062c4380d33117a8e12ed041a321859f5e4da
pedrolisboaa <phlisboa2000@gmail.com>
2024-05-07 22:39:42 -0300

    atualizando aulas

---
+++ 1  - Iniciando/004_exercio.py
@@ -0,0 +1,13 @@
+nome = 'Pedro'
+sobrenome = 'Lisboa'
+idade = 32
+ano_nascimento = 2024 - idade
+maior_de_idade = idade >= 18
+altura_metro = 1.8
+
+print(f'Nome: {nome}')
+print(f'Sobrenome: {sobrenome}')
+print(f'Idade: {idade}')
+print(f'Ano de nascimento:{ano_nascimento}')
+print(f'É maior de idade? {maior_de_idade}')
+print(f'Altura emmetros: {altura_metro:.2f}')
---
+++ 1  - Iniciando/005_operadores.pyt
@@ -0,0 +1,11 @@
+adicao = 10 + 10
+subtracao = 10 - 5
+mutiplicacao = adicao * subtracao
+divisao = mutiplicacao / adicao
+resto_de_divisao_ou_modulo = 11 % 2
+
+print(adicao)
+print(subtracao)
+print(mutiplicacao)
+print(divisao)
+print(resto_de_divisao_ou_modulo)
